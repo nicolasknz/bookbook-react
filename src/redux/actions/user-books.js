7ae21c5060f48f9f28365cceb4ffab256b1b508d
brunocamposalves <brunocamposalves@hotmail.com>
2020-09-16 09:17:02 -0300

    refactor: I added the result of the api in the state

--- src/redux/actions/user-books.js
+++ src/redux/actions/user-books.js
@@ -2,11 +2,25 @@ import { ADD_BOOK, WITHDRAW_BOOK } from './types';
 import axios from 'axios';
 
 export const requestBook = (newBook, session) => (dispatch) => {
-  console.log(session);
+  console.log(newBook);
+  const book = {
+    title: newBook.title,
+    author: String(newBook.authors),
+    shelf: 1,
+    image_url: newBook.imageLinks.thumbnail,
+    grade: 0,
+    categories: String(newBook.categories),
+    review: 'Não possui avaliação',
+    google_book_id: '1',
+  };
   axios
-    .post(`https://ka-users-api.herokuapp.com/users/${session.user.id}/books`, {
-      headers: { Authorization: session.token },
-    })
+    .post(
+      `https://ka-users-api.herokuapp.com/users/${session.user.id}/books/`,
+      { book },
+      {
+        headers: { Authorization: session.token },
+      }
+    )
     .then((res) => console.log(res.data));
 };
 
