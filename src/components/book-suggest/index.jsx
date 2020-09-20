c4947c7e79e36c03e97453577e60e89e5257dc45
brunocamposalves <brunocamposalves@hotmail.com>
2020-09-20 20:11:40 -0300

    fix: change profile bugs

--- src/components/book-suggest/index.jsx
+++ src/components/book-suggest/index.jsx
@@ -30,12 +30,12 @@ const BookSuggest = () => {
       axios
         .get(`https://www.googleapis.com/books/v1/volumes?q=${session.user.about}`)
         .then((res) => {
-          setNewSuggestBooks(res.data.items);
           setLoading(false);
+          setNewSuggestBooks(res.data.items);
         })
         .catch((error) => console.log(error));
     }
-  }, [session]);
+  }, [session.user.about]);
 
   return (
     <>
--- src/components/styled/styled-menu/index.js
+++ src/components/styled/styled-menu/index.js
@@ -1,13 +1,17 @@
-import styled from "styled-components";
+import styled from 'styled-components';
 
 const StyledMenu = styled.div`
   background-color: #db434d;
   padding: 0px 20px;
   height: 70px;
   margin-bottom: 30px;
+  position: -webkit-sticky;
+  position: sticky;
+  top: -10px;
 
   @media (max-width: 480px) {
     padding: 0px 10px;
+    position: static;
   }
 `;
 
