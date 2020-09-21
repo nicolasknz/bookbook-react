f92cea309bd3a034b412e2d831220ff0a8b79fd4
Willian Brusch <willian.bruschh@gmail.com>
2020-09-21 14:38:14 -0300

    chore: component test

--- src/components/book-feedback/__tests__/index.jsx
+++ src/components/book-feedback/__tests__/index.jsx
@@ -6,6 +6,7 @@ import * as redux from 'react-redux'
 const spy = jest.spyOn(redux, 'useSelector')
 spy.mockReturnValue({ session: 'test' })
 
+
 describe("Render test", () => {
   it('renders correctly', () => {
     const tree = renderer
@@ -25,16 +26,3 @@ describe("Render test", () => {
   });
 
 });
-
-// describe("test callBack", () => {
-//   it('renders correctly callBack', () => {
-//     const mockCallback = jest.fn()
-//     const wrapper = shallow(<BookFeedback book={book} onClick={mockCallback} />)
-
-//     wrapper.find("ModalButton").at(1).simulate("click");
-
-//     expect(mockCallback).toHaveBeenCalledWith(2);
-//     expect(mockCallback).toHaveBeenCalled();
-
-//   });
-// });
